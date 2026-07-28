97ce74d7a00e463acaf456c9b1e2426f8b18d667
Harshul Aggarwal <harshulaggarwal004@gmail.com>
2026-07-28 23:19:51 +0530

    Perf: Enable code splitting and adjust chunk sizing limits

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
-  plugins: [react()],
+plugins: [react()],
+build: {
+  chunksSizeWarningLimit: 1000,
+  rolldownOptions: {
+    output: {
+      codeSplitting: true
+
+    }
+  }
+}
 })
